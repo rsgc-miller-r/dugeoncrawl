064a912b2b16e36f33c44f3bf55ff2024072bd3c
rsgc-miller-r <NumberOne@NumberOnes-MacBook-Pro.local>
2016-01-28 10:52:28 -0500

    started
    yeah. creating maze size values

--- dugeoncrawl/main.swift
+++ dugeoncrawl/main.swift
@@ -7,6 +7,28 @@
 //
 
 import Foundation
+import Darwin
+import Cocoa
 
-print("Hello, World!")
+let endSquare = 200
 
+var maze = [Int](count: endSquare + 1, repeatedValue :0)
+
+repeat {
+
+print("    ____  __  ___   __________________  _   ____________  ___ _       ____")
+print("   / __ |/ / / / | / / ____/ ____/ __ |/ | / / ____/ __ |/   | |     / / /")
+print("  / / / / / / /  |/ / / __/ __/ / / / /  |/ / /   / /_/ / /| | | /| / / /")
+print(" / /_/ / /_/ / /|  / /_/ / /___/ /_/ / /|  / /___/ _, _/ ___ | |/ |/ / /___")
+print("/_____/|____/_/ |_/|____/_____/|____/_/ |_/|____/_/ |_/_/  |_|__/|__/_____/")
+print("")
+print("____________________________GorrillaRIBS 2016______________________________")
+
+
+print("START\n")
+
+print("LOAD\n")
+
+print("EXIT\n")
+
+}
