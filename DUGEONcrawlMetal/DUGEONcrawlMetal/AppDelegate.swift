0299b3e2f43cd7b4e89ce47d7af7afce0ad50c9b
rsgc-miller-r <NumberOne@NumberOnes-MacBook-Pro.local>
2016-02-01 09:54:38 -0500

    Metal
    Going to try using the metal graphics library as well, looks interesting

---
+++ DUGEONcrawlMetal/DUGEONcrawlMetal/AppDelegate.swift
@@ -0,0 +1,28 @@
+//
+//  AppDelegate.swift
+//  DUGEONcrawlMetal
+//
+//  Created by NumberOne on 2016-02-01.
+//  Copyright (c) 2016 NumberOne. All rights reserved.
+//
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+    
+    @IBOutlet weak var window: NSWindow!
+    
+    func applicationDidFinishLaunching(aNotification: NSNotification) {
+        
+    }
+
+    func applicationWillTerminate(aNotification: NSNotification) {
+        
+    }
+    
+    func applicationShouldTerminateAfterLastWindowClosed(sender: NSApplication) -> Bool {
+        return true
+    }
+    
+}
